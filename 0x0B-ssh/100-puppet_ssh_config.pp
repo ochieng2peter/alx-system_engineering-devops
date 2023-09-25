008b2cd0f6d3e4d92dd7360ef0fe1b567c652702
ochieng2peter <ochieng2.peter@gmail.com>
2023-09-26 01:59:02 +0300

    0x0B-ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,40 @@
-# Client configuration file (w/ Puppet)
+# using puppet to make changes to the default ssh config file
+# so that one can connect to a server without typing a password.
 
-file { '/home/stephen/.ssh/config':
-  ensure  => file,
-  owner   => 'stephen',
-  group   => 'stephen',
-  mode    => '0600',
-  content => template('~/.ssh/school/ssh_config.erb'),
+include stdlib
+
+file_line { 'SSH Private Key':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true
 }
+
+# Regex match explanation
+#
+# ^       beginning of the line
+# [#]*  atleast one hash character
+# [\s]*  zero or more white space characters
+# (?i)IdentityFile case insensitive "IdentityFile"
+# [\s]+ at least one whitespace character
+# ~/.ssh/id_rsa The ssh private key file path we want to replace
+# $      end of the line
+
+file_line { 'Deny Password Auth':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true
+}
+
+# Regex match explanation
+#
+# ^       beginning of the line
+# [#]*  atleast one hash character
+# [\s]*  zero or more white space characters
+# (?i)PasswordAuthentication case insensitive "PasswordAuthentication"
+# [\s]+ at least one whitespace character
+# (yes|no) with the value "yes" or the value "no"
+# $      end of the line
